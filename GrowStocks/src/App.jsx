14992ee253ffd69e23d242194483080e006bbb21
Samved Suresh Jain <samvedsjain2005@gmail.com>
2024-10-12 17:13:18 +0530

    Changed import

--- GrowStocks/src/App.jsx
+++ GrowStocks/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import Home from './components/Home'
+import Hero from './components/Hero'
 
 export default function App() {
   
@@ -10,6 +10,7 @@ export default function App() {
       <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
       {/* <Home/> */}
       <Navbar/>
+      <Hero/>
       </div>
     </>
   )
